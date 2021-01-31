98065fa060aeb4e60cf58fd0176acd625134b214
mac <mac@macs-iMac.local>
2021-01-31 20:28:06 +0600

    Initial Commit

---
+++ QuizFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizFinal
+//
+//  Created by mac on 1/31/21.
+//  Copyright © 2021 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
